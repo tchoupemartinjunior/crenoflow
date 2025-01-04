c6a4c347ce32613f33346f633dc4a31ef29316d5
MJ_Tchoupe <tchoupemartinjunior@gmail.com>
2025-01-04 16:49:56 +0100

    app creation with keycloak

--- src/test/java/com/tchoupe/crenoflow/security/oauth2/AudienceValidatorTest.java
+++ src/test/java/com/tchoupe/crenoflow/security/oauth2/AudienceValidatorTest.java
@@ -2,6 +2,7 @@ package com.tchoupe.crenoflow.security.oauth2;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.Arrays;
