cc4a95bfdc645408e817d08bc44b73556cf8d044
MJ_Tchoupe <tchoupemartinjunior@gmail.com>
2025-01-09 01:39:07 +0100

    create subject form

---
+++ src/main/webapp/app/shared/teacherForms/teacher-subjects-form/teacher-subjects-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jhi-teacher-subjects-form',
+  templateUrl: './teacher-subjects-form.component.html',
+  styleUrls: ['./teacher-subjects-form.component.scss'],
+})
+export class TeacherSubjectsFormComponent {}
