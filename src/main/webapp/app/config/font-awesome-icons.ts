df7d7f91e477a62be6e94989f8c7c965a65aad5f
MJ_Tchoupe <tchoupemartinjunior@gmail.com>
2025-01-15 00:49:36 +0100

    home page add icons

--- src/main/webapp/app/config/font-awesome-icons.ts
+++ src/main/webapp/app/config/font-awesome-icons.ts
@@ -40,6 +40,7 @@ import {
   faPersonChalkboard,
   faMessage,
   faCamera,
+  faSliders,
   // jhipster-needle-add-icon-import
 } from '@fortawesome/free-solid-svg-icons';
 
@@ -85,5 +86,6 @@ export const fontAwesomeIcons = [
   faPersonChalkboard,
   faMessage,
   faCamera,
+  faSliders,
   // jhipster-needle-add-icon-import
 ];
